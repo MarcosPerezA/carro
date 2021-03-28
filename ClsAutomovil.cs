f50f4e3a67a0b06cf9d33f58f7fdfbd21b39852c
MarcosPerezA <mpereza33@miumg.edu.gt>
2021-03-28 17:02:06 -0600

    .

---
+++ ClsAutomovil.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_7_poo_inicio.clases
+{
+    class ClsAutomovil
+    {
+
+        public string marca { get; }//quitamos el st para que no se pueda cambiar la marca
+        public string apagar { get; }
+        public int vel_max { get; } //si pongo private en vez de public no lo puede acceder el usuario
+        private int velocidadActual { get; set; } //si pongo private no lo puede acceder el usuario
+        public string color { get; set; }
+        private bool encendido = false;
+        private bool radio = true;
+        private bool elegir=true;
+        private int vel_min = 0;
+          
+        public string Acelerar()
+        {
+            if (velocidadActual < vel_max)
+            {
+                velocidadActual = velocidadActual + 10;
+                return $"vas a {velocidadActual} km por hora";
+            }
+            else
+            {
+                return " has llegado a la velocidad maxima ";
+            }
+        }
+        public bool EstaEncendido()
+        {
+            return encendido;
+        }
+        public bool radioencendida()
+        {
+            return radio;
+        }
+        public bool elige()
+        {
+            return elegir;
+        }
+        public ClsAutomovil(string marcaCarro, int velocidadMx)//creando clase constructor para agregar valores al carro que el usuario no pueda modificar porque esta en pivate
+        {
+            this.marca = marcaCarro;
+            this.vel_max = velocidadMx;
+        }
+
+        public ClsAutomovil()
+        {
+        }
+
+        public string EncenderMotor()
+        {
+            if (encendido)
+            {
+                return "el carro ya esta encendido ";
+            }
+            else
+            {
+               encendido = true;
+                velocidadActual = 0;
+                return "Listo!! carro encendido";
+            }
+            
+        }
+        public string Frenar()
+        {
+            
+            if (velocidadActual>vel_min)
+            {
+                velocidadActual = velocidadActual - 10;
+                return $"vas a {velocidadActual} km por hora";
+            } else
+            {
+                return " Tienes que acelerar de nuevo ";
+            }
+
+          
+            
+        }
+
+        
+        public string ApagarMotor()
+        {
+
+            if (velocidadActual > 0)
+            {
+                return "NO APAGUE EL CARRO MIENTRAS ESTA EN MOVIMIENTO PUEDE CAUSAR UN ACCIDENTE";
+            }
+            else
+            {
+                return "ha apagado el carro";
+            }
+           
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using clase_7_poo_inicio.clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+
+namespace clase_7_poo_inicio
+{
+    public partial class Form1 : Form
+    {
+        ClsAutomovil carrito; //lo pongo aqui para que me permita usarlo en todas las clases
+        private string ruta="";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonInicio_Click(object sender, EventArgs e)
+        {
+            //crear el carro
+           carrito = new ClsAutomovil("Meches",150);
+            carrito.color = "Negro";
+            MessageBox.Show("tu carro color negro ha sido creado ");
+            
+        
+            //string respuesta = carrito.EncenderMotor();
+            //labeldatos.Text = "encendido";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (carrito.EstaEncendido())
+            {
+                labelVelocidad.Text = carrito.Acelerar();
+            }
+            else
+            {
+                MessageBox.Show($"lo lamento el carro{carrito.marca} esta apagado");
+            }
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = "C:/Users/W10/Downloads/aceleracion.wav";
+            player.Play();
+
+        }
+
+        private void encender_Click(object sender, EventArgs e)//clase de encender
+        {
+            
+
+            string respuesta = carrito.EncenderMotor();
+            labeldatos.Text = respuesta;
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = "C:/Users/W10/Downloads/encendido.wav";
+            player.Play();
+
+        }
+
+        private void frenar_Click(object sender, EventArgs e)
+        {
+            
+            if (carrito.EstaEncendido())
+            {
+                labelVelocidad.Text = carrito.Frenar();
+            }else
+            {
+                
+            }
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = "C:/Users/W10/Downloads/freno.wav";
+            player.Play();
+        }
+
+        private void labelVelocidad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void apagar_Click(object sender, EventArgs e)
+        {
+            string apagar = carrito.ApagarMotor();
+            apago.Text = carrito.ApagarMotor();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void ccarro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radio_Click(object sender, EventArgs e)
+        {
+            if (carrito.EstaEncendido())
+            {
+                MessageBox.Show("Listo, radio encendida");
+            }
+            else
+            {
+                   
+            }
+        }
+
+        private void apradio_Click(object sender, EventArgs e)
+        {
+           
+                axWindowsMediaPlayer1.Ctlcontrols.stop();
+               
+            
+            
+        }
+
+        private void playlist_Click(object sender, EventArgs e)
+        {
+            if(carrito.EstaEncendido())
+            {
+                
+                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        ruta = openFileDialog1.FileName;
+                        lblru.Text = ruta;
+                    }
+                
+                else
+                {
+                    
+                }
+               
+            }
+        }
+        private void rep_Click(object sender, EventArgs e)
+        {
+            if (carrito.elige())
+            {
+                axWindowsMediaPlayer1.URL = ruta;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void pau_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+    }
+}
